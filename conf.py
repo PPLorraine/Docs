187bbb322133b6c075c3e18ee66714c1b40ad048
Valentin Lorentz <progval@progval.net>
2012-11-19 19:51:05 +0100

    Utilisation de la licence CC-BY.

--- conf.py
+++ conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Documentation du Parti Pirate Lorraine'
-copyright = u'2012, Parti Pirate Lorraine'
+copyright = u'2012, Parti Pirate Lorraine (licence CC-BY)'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
